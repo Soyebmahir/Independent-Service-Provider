079ddd02b713e39db384dc8e9a39c36fd48bedaa
Soyeb <tssoyeb@gmail.com>
2022-04-17 12:20:19 +0600

    display fetched data at ui and responsive the home page

--- src/component/Home/Service/Service.js
+++ src/component/Home/Service/Service.js
@@ -1,9 +1,21 @@
 import React from 'react';
 
-const Service = () => {
-    return (
-        <div>
+const Service = ({ service }) => {
 
+    const { name, price, img, description } = service;
+    return (
+        <div className='col gy-4'>
+            <div className="card ">
+                <img src={img} style={{ "height": "300px" }} className=" w-100 card-img-top" alt="..." />
+                <div className="card-body">
+                    <h5 className="card-title">{name}</h5>
+                    <p className="card-text">Price : {price} BDT</p>
+                    <p className="card-text"><small>{description}</small></p>
+                </div>
+                <div className="card-footer">
+                    <small className="text-muted">Last updated 3 mins ago</small>
+                </div>
+            </div>
         </div>
     );
 };
--- src/component/Home/Services/Services.js
+++ src/component/Home/Services/Services.js
@@ -1,11 +1,22 @@
-import React from 'react';
+
 import useServices from '../../../Hooks/useServices/useServices';
 
+import Service from '../Service/Service';
+
 const Services = () => {
-    const { services } = useServices()
+    const [services] = useServices()
     console.log(services);
     return (
-        <div>
+        <div className='container'>
+            <h2 className='text-info text-center'>{services.length} Services Available</h2>
+
+            <div className=' row row-cols-1 row-cols-md-2 row-cols-lg-3'>
+                {
+                    services.map((service) => <Service
+                        key={service.id}
+                        service={service}></Service>)
+                }
+            </div>
 
         </div>
     );
