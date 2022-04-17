c6ecca1be560998592ae70a0d75c70003478a2bf
Soyeb <tssoyeb@gmail.com>
2022-04-17 11:05:16 +0600

    create some component and add browserRouter

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 
 import './App.css';
+import Header from './Shared/Header/Header';
 
 function App() {
   return (
     <div className="App">
-      <h1>Hellow There</h1>
+      <Header></Header>
     </div>
   );
 }
